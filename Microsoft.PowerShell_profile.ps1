6c2a0a25e851ace7f4dba80f323d29d3361a8c8a
Ashit Milne <ashitmilne1969@protonmail.com>
2025-09-01 18:36:16 -0400

    EOD update 2025-09-01 18:36

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+# Always start in your project directory
+Set-Location "C:\Users\Ash\Documents\Planet III"
---
+++ push-nightly.ps1
@@ -0,0 +1,4 @@
+cd "C:\Users\Ash\Documents\Planet III"
+git add .
+git commit -m "EOD update $(Get-Date -Format 'yyyy-MM-dd HH:mm')"
+git push origin main
