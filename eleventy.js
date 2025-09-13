5227378bb371338349d01d1b6887905411607ddf
Ashit Milne <ashitmilne1969@protonmail.com>
2025-09-13 17:02:40 -0400

    Nightly commit: 2025-09-13 17:02:40.84

--- eleventy.js
+++ eleventy.js
@@ -1,14 +1,10 @@
 module.exports = function(eleventyConfig) {
-  // This line tells Eleventy to copy the 'assets' folder
-  // and everything in it to the final website ('_site') folder.
-  eleventyConfig.addPassthroughCopy("assets");
-
-  // This is the standard configuration for your project's
-  // input and output directories.
   return {
     dir: {
-      input: ".",
-      output: "_site"
+      input: "planet-III", // The folder where your source files (like .md, .liquid) are located
+      output: "_site",            // The folder where the built website is created
+      includes: "_partials",      // The folder for your layouts and includes
+      data: "_data"               // The folder for your global data files
     }
   };
-};
+};	
